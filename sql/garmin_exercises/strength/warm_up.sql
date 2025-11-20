5c6c357ad705104f6ca403a40a10c2d2cd51d57e
Greg Garnhart <greg.garnhart12@gmail.com>
2025-11-19 20:56:19 -0500

    strength done

---
+++ sql/garmin_exercises/strength/warm_up.sql
@@ -0,0 +1,123 @@
+-- Garmin Exercises: WARM_UP
+
+-- Total exercises: 39
+
+-- Generated: 2025-11-18T02:24:16.102Z
+
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'NECK_ROTATIONS', '', '{core,obliques,mobility,flexibility,warmup}', '{bodyweight}', 'Neck Rotations is a warm up exercise targeting the core. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STANDING_HAMSTRING_STRETCH', '', '{legs,hamstrings,mobility,flexibility,warmup}', '{rings}', 'Standing Hamstring Stretch is a warm up exercise targeting the hamstrings. Performed using rings.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_90_90', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch 90 90 is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_ABS', 'STRENGTH', '{core,abs,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Abs is a warm up exercise targeting the core, abs. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_BUTTERFLY', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Butterfly is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_CALF', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Calf is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_CAT_COW', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Cat Cow is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_CHILDS_POSE', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Childs Pose is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_COBRA', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Cobra is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_FOREARMS', 'STRENGTH', '{forearms,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Forearms is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_FORWARD_GLUTES', 'STRENGTH', '{glutes,hips,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Forward Glutes is a warm up exercise targeting the glutes. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_FRONT_SPLIT', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Front Split is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_HAMSTRING', 'STRENGTH', '{legs,hamstrings,mobility,flexibility,warmup}', '{rings}', 'Stretch Hamstring is a warm up exercise targeting the hamstrings. Performed using rings.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_HIP_FLEXOR_AND_QUAD', 'STRENGTH', '{legs,quads,glutes,hips,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Hip Flexor And Quad is a warm up exercise targeting the quadriceps, glutes. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LAT', 'STRENGTH', '{back,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lat is a warm up exercise targeting the back. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LEVATOR_SCAPULAE', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Levator Scapulae is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LUNGE_WITH_SPINAL_TWIST', 'STRENGTH', '{lunge pattern,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lunge With Spinal Twist is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LUNGING_HIP_FLEXOR', 'STRENGTH', '{glutes,hips,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lunging Hip Flexor is a warm up exercise targeting the glutes. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LYING_ABDUCTION', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lying Abduction is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LYING_IT_BAND', 'STRENGTH', '{mobility,flexibility,warmup}', '{resistance band}', 'Stretch Lying It Band is a warm up exercise. Performed using resistance band.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LYING_KNEE_TO_CHEST', 'STRENGTH', '{chest,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lying Knee To Chest is a warm up exercise targeting the chest. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LYING_PIRIFORMIS', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lying Piriformis is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_LYING_SPINAL_TWIST', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Lying Spinal Twist is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_NECK', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Neck is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_OBLIQUES', 'STRENGTH', '{core,obliques,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Obliques is a warm up exercise targeting the core. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_OVER_UNDER_SHOULDER', 'STRENGTH', '{shoulders,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Over Under Shoulder is a warm up exercise targeting the shoulders. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_PECTORAL', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Pectoral is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_PIGEON_POSE', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Pigeon Pose is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_PIRIFORMIS', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Piriformis is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_QUAD', 'STRENGTH', '{legs,quads,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Quad is a warm up exercise targeting the quadriceps. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_SCORPION', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Scorpion is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_SHOULDER', 'STRENGTH', '{shoulders,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Shoulder is a warm up exercise targeting the shoulders. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_SIDE', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Side is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_SIDE_LUNGE', 'STRENGTH', '{lunge pattern,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Side Lunge is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_SIDE_SPLIT', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Side Split is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_STANDING_IT_BAND', 'STRENGTH', '{mobility,flexibility,warmup}', '{resistance band}', 'Stretch Standing It Band is a warm up exercise. Performed using resistance band.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_STRADDLE', 'STRENGTH', '{mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Straddle is a warm up exercise. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_TRICEPS', 'STRENGTH', '{triceps,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Triceps is a warm up exercise targeting the triceps. This is a bodyweight exercise.');
+
+INSERT INTO garmin_exercises (exercise_category, exercise_name, type, tags, equipment, description)
+VALUES ('WARM_UP', 'STRETCH_WALL_CHEST_AND_SHOULDER', 'STRENGTH', '{chest,shoulders,mobility,flexibility,warmup}', '{bodyweight}', 'Stretch Wall Chest And Shoulder is a warm up exercise targeting the chest, shoulders. This is a bodyweight exercise.');
